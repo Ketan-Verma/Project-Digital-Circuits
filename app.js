62f0e9af11f1e36f84eb050f498432ce5967dd3a
Ketan <68579341+Ketan-Verma@users.noreply.github.com>
2022-11-22 01:48:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,273 +1,76 @@
-let selected = "none";
+let buffer1 = new Buffer(50, 50);
+let not1 =new NOT(50,150);
+let and1 = new AND(50,250);
+let nand1 = new NAND(50,350);
+let or1 = new OR(150,300);
+let nor1 = new NOR(300,50);
+let xnor1 = new XNOR(300,350);
+let xor1 = new XOR(400,250);
+let switch1 = new Switch(200,200)
 // wire,
 //buffer,led,NOT,AND,OR,NAND,NOR,XOR,XNOR
-
+// let wire1 = new WIRE(nand1.node1,nand1.node2);
+// let wire2 = new WIRE(and1.node1,switch1.node4);
+// let wire3 = new WIRE(nand1.node3,and1.node2);
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
 function setup() {
     createCanvas(600, 400);
     background(220);
-    console.log("hello")
+}
 
-    let buffer1 = new Buffer(100, 100)
+function draw() {
+    // frameRate(10);
+    // scale(0.75);
+    cursor("auto");
+    if(moveActive)cursor("move")
+    background(220);
+    for (let index = 0; index < wires.length; index++) {
+        const element = wires[index];
+        element.show();
+        element.update();
+    }
+    if(wireNode.length>0){
+        line(wireNode[0].x,wireNode[0].y,mouseX,mouseY);
+    }
+    // wire1.show();
+    // wire2.show();
+    // wire3.show();
+    // wire1.update();
+    // wire2.update();
+    // wire3.update();
+
+    switch1.show()
+    switch1.dragThis()
+    
+    buffer1.update();
     buffer1.show();
-    let not1 =new NOT(100,150)
+    buffer1.dragThis();
+    not1.update()
     not1.show()
-    let and1 = new AND(100,200);
+    not1.dragThis();
+    and1.update();
     and1.show();
-    let nand1 = new NAND(100,250);
-    nand1.show();
-    let or1 = new OR(100,300);
+    and1.dragThis();
+    nand1.update();     
+    nand1.show();     
+    nand1.dragThis();     
+    or1.update();
     or1.show();
-    let nor1 = new NOR(100,350);
+    or1.dragThis();
+    nor1.update();
     nor1.show();
-    let xor1 = new XOR(200,100);
+    nor1.dragThis();
+    xor1.update();
     xor1.show();
-    let xnor1 = new XNOR(200,100);
+    xor1.dragThis();
+    xnor1.update();
     xnor1.show();
+    xnor1.dragThis();
 
 }
 
-function draw() {}
 
-class Buffer {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    show() {
-        line(this.x, this.y, this.x + 15, this.y);
-        line(this.x, this.y + 20, this.x + 15, this.y + 20);
-        line(this.x + 50, this.y + 10,this.x + 65, this.y + 10);
-        beginShape();
-        vertex(this.x + 15, this.y-10);
-        vertex(this.x + 15, this.y + 30);
-        vertex(this.x + 50, this.y + 10);
-        endShape(CLOSE);
-        circle(this.x-5, this.y, 10);
-        circle(this.x-5, this.y + 20, 10);
-        circle(this.x + 70, this.y + 10, 10);
-    }
-}
-class NOT {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    show() {
-        line(this.x, this.y, this.x + 15, this.y);
-        line(this.x, this.y + 20, this.x + 15, this.y + 20);
-        line(this.x + 50, this.y + 10,this.x + 70, this.y + 10);
-        beginShape();
-        vertex(this.x + 15, this.y-10);
-        vertex(this.x + 15, this.y + 30);
-        vertex(this.x + 50, this.y + 10);
-        endShape(CLOSE);
-        circle(this.x-5, this.y, 10);
-        circle(this.x-5, this.y + 20, 10);
-        circle(this.x + 75, this.y + 10, 10);
-        circle(this.x + 55, this.y + 10, 8);
-    }
-}
-class AND{
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    show() {
-        line(this.x, this.y, this.x + 15, this.y);
-        line(this.x, this.y + 20, this.x + 15, this.y + 20);
-        line(this.x + 40, this.y + 10,this.x + 65, this.y + 10);
-        // beginShape();
-        // vertex(this.x + 15, this.y-10);
-        // vertex(this.x + 15, this.y + 30);
-        // vertex(this.x + 50, this.y + 10);
-        // endShape(CLOSE);
-        arc(this.x + 15, this.y + 10, 60, 40,-PI/2,PI/2,CHORD);
-        circle(this.x-5, this.y, 10);
-        circle(this.x-5, this.y + 20, 10);
-        circle(this.x + 70, this.y + 10, 10);
-    }
-}
-class NAND{
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    show() {
-        line(this.x, this.y, this.x + 15, this.y);
-        line(this.x, this.y + 20, this.x + 15, this.y + 20);
-        line(this.x + 50, this.y + 10,this.x + 65, this.y + 10);
-        // beginShape();
-        // vertex(this.x + 15, this.y-10);
-        // vertex(this.x + 15, this.y + 30);
-        // vertex(this.x + 50, this.y + 10);
-        // endShape(CLOSE);
-        arc(this.x + 15, this.y + 10, 60, 40,-PI/2,PI/2,CHORD);
-        circle(this.x-5, this.y, 10);
-        circle(this.x-5, this.y + 20, 10);
-        circle(this.x + 70, this.y + 10, 10);
-        circle(this.x + 50, this.y + 10, 8);
-    }
-}
-class OR{
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.mag = 5;
-    }
-    show() {
-        line(this.x, this.y, this.x + 20, this.y);
-        line(this.x, this.y + 20, this.x + 20, this.y + 20);
-        line(this.x + 40, this.y + 10,this.x + 65, this.y + 10);
-        beginShape();
-        vertex(this.x + 40,this.y+10)//e
-        vertex(this.x + 35,this.y+5 - this.mag)//d
-        vertex(this.x + 30,this.y - this.mag)//c
-        vertex(this.x + 20, this.y-5 - this.mag)//b3
-        vertex(this.x + 15, this.y-5 - this.mag)//b
-        vertex(this.x + 10, this.y-5 - this.mag)//b2
-        vertex(this.x + 15, this.y - this.mag)//a
-        vertex(this.x + 18,this.y+5 - this.mag)//center
-        vertex(this.x + 20,this.y+10)//0
-        vertex(this.x + 18,this.y+15+this.mag)//center
-        vertex(this.x + 15, this.y + 20+this.mag)//a
-        vertex(this.x + 10, this.y + 25+this.mag)//b2
-        vertex(this.x + 15, this.y + 25+this.mag)//b
-        vertex(this.x + 20, this.y + 25+this.mag)//b3
-        vertex(this.x + 30,this.y+20+this.mag)//c
-        vertex(this.x + 35,this.y+15+this.mag)//d
-        vertex(this.x + 40,this.y+10)//e
-        endShape();
-        circle(this.x-5, this.y, 10);
-        circle(this.x-5, this.y + 20, 10);
-        circle(this.x + 70, this.y + 10, 10);
-    }
-}
-class NOR{
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.mag = 5;
-    }
-    show() {
-        line(this.x, this.y, this.x + 20, this.y);
-        line(this.x, this.y + 20, this.x + 20, this.y + 20);
-        line(this.x + 40, this.y + 10,this.x + 65, this.y + 10);
-        beginShape();
-        vertex(this.x + 40,this.y+10)//e
-        vertex(this.x + 35,this.y+5 - this.mag)//d
-        vertex(this.x + 30,this.y - this.mag)//c
-        vertex(this.x + 20, this.y-5 - this.mag)//b3
-        vertex(this.x + 15, this.y-5 - this.mag)//b
-        vertex(this.x + 10, this.y-5 - this.mag)//b2
-        vertex(this.x + 15, this.y - this.mag)//a
-        vertex(this.x + 18,this.y+5 - this.mag)//center
-        vertex(this.x + 20,this.y+10)//0
-        vertex(this.x + 18,this.y+15+this.mag)//center
-        vertex(this.x + 15, this.y + 20+this.mag)//a
-        vertex(this.x + 10, this.y + 25+this.mag)//b2
-        vertex(this.x + 15, this.y + 25+this.mag)//b
-        vertex(this.x + 20, this.y + 25+this.mag)//b3
-        vertex(this.x + 30,this.y+20+this.mag)//c
-        vertex(this.x + 35,this.y+15+this.mag)//d
-        vertex(this.x + 40,this.y+10)//e
-        endShape();
-        circle(this.x-5, this.y, 10);
-        circle(this.x-5, this.y + 20, 10);
-        circle(this.x + 70, this.y + 10, 10);
-        circle(this.x + 44, this.y + 10, 7);
-    }
-}
-class XOR{
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.mag = 5;
-    }
-    show() {
-        line(this.x, this.y, this.x + 20, this.y);
-        line(this.x, this.y + 20, this.x + 20, this.y + 20);
-        line(this.x + 40, this.y + 10,this.x + 65, this.y + 10);
-        beginShape();
-        vertex(7+this.x + 40,this.y+10)//e
-        vertex(7+this.x + 35,this.y+5 - this.mag)//d
-        vertex(7+this.x + 30,this.y - this.mag)//c
-        vertex(7+this.x + 20, this.y-5 - this.mag)//b3
-        vertex(7+this.x + 15, this.y-5 - this.mag)//b
-        vertex(7+this.x + 10, this.y-5 - this.mag)//b2
-        vertex(7+this.x + 15, this.y - this.mag)//a
-        vertex(7+this.x + 18,this.y+5 - this.mag)//center
-        vertex(7+this.x + 20,this.y+10)//0
-        vertex(7+this.x + 18,this.y+15+this.mag)//center
-        vertex(7+this.x + 15, this.y + 20+this.mag)//a
-        vertex(7+this.x + 10, this.y + 25+this.mag)//b2
-        vertex(7+this.x + 15, this.y + 25+this.mag)//b
-        vertex(7+this.x + 20, this.y + 25+this.mag)//b3
-        vertex(7+this.x + 30,this.y+20+this.mag)//c
-        vertex(7+this.x + 35,this.y+15+this.mag)//d
-        vertex(7+this.x + 40,this.y+10)//e
-        endShape();
-        push();
-        noFill()
-        beginShape();
-        vertex(2+this.x + 10, this.y-5 - this.mag)//b2
-        vertex(2+this.x + 15, this.y - this.mag)//a
-        vertex(2+this.x + 18,this.y+5 - this.mag)//center
-        vertex(2+this.x + 20,this.y+10)//0
-        vertex(2+this.x + 18,this.y+15+this.mag)//center
-        vertex(2+this.x + 15, this.y + 20+this.mag)//a
-        vertex(2+this.x + 10, this.y + 25+this.mag)//b2
-        endShape();
-        pop();
-        circle(this.x-5, this.y, 10);
-        circle(this.x-5, this.y + 20, 10);
-        circle(this.x + 70, this.y + 10, 10);
-    }
-}
-class XNOR{
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.mag = 5;
-    }
-    show() {
-        line(this.x, this.y, this.x + 20, this.y);
-        line(this.x, this.y + 20, this.x + 20, this.y + 20);
-        line(this.x + 40, this.y + 10,this.x + 65, this.y + 10);
-        beginShape();
-        vertex(7+this.x + 40,this.y+10)//e
-        vertex(7+this.x + 35,this.y+5 - this.mag)//d
-        vertex(7+this.x + 30,this.y - this.mag)//c
-        vertex(7+this.x + 20, this.y-5 - this.mag)//b3
-        vertex(7+this.x + 15, this.y-5 - this.mag)//b
-        vertex(7+this.x + 10, this.y-5 - this.mag)//b2
-        vertex(7+this.x + 15, this.y - this.mag)//a
-        vertex(7+this.x + 18,this.y+5 - this.mag)//center
-        vertex(7+this.x + 20,this.y+10)//0
-        vertex(7+this.x + 18,this.y+15+this.mag)//center
-        vertex(7+this.x + 15, this.y + 20+this.mag)//a
-        vertex(7+this.x + 10, this.y + 25+this.mag)//b2
-        vertex(7+this.x + 15, this.y + 25+this.mag)//b
-        vertex(7+this.x + 20, this.y + 25+this.mag)//b3
-        vertex(7+this.x + 30,this.y+20+this.mag)//c
-        vertex(7+this.x + 35,this.y+15+this.mag)//d
-        vertex(7+this.x + 40,this.y+10)//e
-        endShape();
-        push();
-        noFill()
-        beginShape();
-        vertex(2+this.x + 10, this.y-5 - this.mag)//b2
-        vertex(2+this.x + 15, this.y - this.mag)//a
-        vertex(2+this.x + 18,this.y+5 - this.mag)//center
-        vertex(2+this.x + 20,this.y+10)//0
-        vertex(2+this.x + 18,this.y+15+this.mag)//center
-        vertex(2+this.x + 15, this.y + 20+this.mag)//a
-        vertex(2+this.x + 10, this.y + 25+this.mag)//b2
-        endShape();
-        pop();
-        circle(this.x-5, this.y, 10);
-        circle(this.x-5, this.y + 20, 10);
-        circle(this.x + 70, this.y + 10, 10);
-    }
-}
-/*
-*/ 
+
+
